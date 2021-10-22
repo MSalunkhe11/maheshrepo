c3bc25f516e3ca71340f54b1185fbadc13d81e2b
mas <mas@gmail.com>
2021-10-22 10:04:48 +0000

    tf file added

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "myawsserver" {
+  ami = "ami-02e136e904f3da870"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "mahesh-aws-ec2-instance-v4 from github"
+    Env = "Dev"
+  }
+}
+
+output "myawsserver-ip" {
+  value = aws_instance.myawsserver.public_ip
+}
+
+output "myserver-PrivateIP" {
+  value = aws_instance.myawsserver.private_ip
+}
+
+
+
